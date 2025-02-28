bb8910ef26d9517307b629e91a441016760b7109
unknown <andre.lljr@live.com>
2025-02-28 16:29:24 -0300

    fix: strings.g added to Barrel File

--- lib/core.dart
+++ lib/core.dart
@@ -5,4 +5,5 @@ export 'package:levy_core/core/inject/inject.dart';
 export 'package:levy_core/core/inject/inject_env.dart';
 // TODO: Remove Theme
 export 'package:levy_core/core/theme/theme.dart';
+export 'package:levy_core/core/utils/i18n/strings.g.dart';
 export 'package:levy_core/core/utils/commons_translation.dart';
